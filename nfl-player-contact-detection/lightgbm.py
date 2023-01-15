ae3ebc0f61fb7d318493cce9618bc2f33a353095
chickenta2ta <tishizuka1906@icloud.com>
2023-01-16 04:29:47 +0900

    use different model for player and ground

--- nfl-player-contact-detection/lightgbm.py
+++ nfl-player-contact-detection/lightgbm.py
@@ -2,6 +2,7 @@ import gc
 import os
 
 import lightgbm as lgb
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
@@ -209,10 +210,6 @@ def create_features(
     labels, columns = calculate_iou(labels)
     feature_columns += columns
 
-    labels["g_flag"] = labels["nfl_player_id_2"] == "G"
-    labels = labels.astype({"g_flag": "int"})
-    feature_columns.append("g_flag")
-
     return labels, feature_columns
 
 
@@ -305,100 +302,206 @@ def add_product_and_difference_features(labels):
     return labels, feature_columns
 
 
-train_labels, feature_columns = create_features(
+def separate_player_and_ground(labels, feature_columns_player):
+    is_ground = labels["nfl_player_id_2"] == "G"
+
+    labels_player = labels[~is_ground].copy()
+    labels_ground = labels[is_ground].copy()
+
+    feature_columns_ground = []
+    for column_name in feature_columns_player:
+        if (
+            "_2" in column_name
+            or column_name == "distance"
+            or column_name.startswith("iou_")
+        ):
+            continue
+        else:
+            feature_columns_ground.append(column_name)
+
+    return labels_player, labels_ground, feature_columns_ground
+
+
+def get_indices_of_closer_than_threshold(labels, feature_columns, threshold=2):
+    if "distance" in feature_columns:
+        is_close = (labels["distance"] <= threshold) | labels["distance"].isnull()
+        return is_close
+    else:
+        return pd.Series(True, index=labels.index)
+
+
+def train_lightgbm(
+    X_train,
+    y_train,
+    param={
+        "objective": "binary",
+        "num_boost_round": 10_000,
+        "learning_rate": 0.03,
+        "device_type": "gpu" if torch.cuda.is_available() else "cpu",
+        "seed": 42,
+        "force_row_wise": True,
+        "early_stopping_round": 100,
+        "metric": "auc",
+    },
+):
+    skf = StratifiedKFold(shuffle=True, random_state=42)
+
+    # This weird implementation is to avoid LightGBM warnings
+    num_boost_round = param["num_boost_round"]
+    stopping_rounds = param["early_stopping_round"]
+
+    param_copy = param.copy()
+    del param_copy["num_boost_round"], param_copy["early_stopping_round"]
+
+    models = []
+    for i, (train_index, test_index) in enumerate(skf.split(X_train, y_train)):
+        print(f"Fold {i + 1}:")
+
+        X_train_fold, y_train_fold = X_train.loc[train_index], y_train[train_index]
+        X_test_fold, y_test_fold = X_train.loc[test_index], y_train[test_index]
+
+        train_data = lgb.Dataset(X_train_fold, label=y_train_fold)
+        validation_data = lgb.Dataset(
+            X_test_fold, label=y_test_fold, reference=train_data
+        )
+
+        model = lgb.train(
+            param_copy,
+            train_data,
+            num_boost_round=num_boost_round,
+            valid_sets=[validation_data],
+            callbacks=[lgb.early_stopping(stopping_rounds=stopping_rounds)],
+        )
+
+        y_pred = model.predict(X_test_fold, num_iteration=model.best_iteration)
+        score = roc_auc_score(y_test_fold, y_pred)
+        print(f"Score: {score}")
+
+        models.append(model)
+
+    return models
+
+
+def predict_lightgbm(
+    models,
+    X_train,
+    y_train,
+    X_test,
+    x0=[0.5],
+):
+    y_pred_train = np.zeros(len(X_train))
+    y_pred_test = np.zeros(len(X_test))
+
+    for model in models:
+        y_pred_train += model.predict(
+            X_train, num_iteration=model.best_iteration
+        ) / len(models)
+        y_pred_test += model.predict(X_test, num_iteration=model.best_iteration) / len(
+            models
+        )
+
+    res = minimize(
+        matthews_corrcoef_, x0, args=(y_train, y_pred_train), method="Nelder-Mead"
+    )
+    y_test = (y_pred_test > res.x[0]).astype("int")
+
+    return y_test
+
+
+train_labels, feature_columns_player = create_features(
     train_labels, train_player_tracking, train_baseline_helmets, train_video_metadata
 )
 
-DISTANCE_THRESHOLD = 2
+is_close = get_indices_of_closer_than_threshold(train_labels, feature_columns_player)
+train_labels = train_labels[is_close]
+train_labels.reset_index(drop=True, inplace=True)
 
-if "distance" in feature_columns:
-    train_labels = train_labels[
-        (train_labels["distance"] <= DISTANCE_THRESHOLD)
-        | (train_labels["distance"].isnull())
-    ]
-    train_labels.reset_index(drop=True, inplace=True)
+(
+    train_labels_player,
+    train_labels_ground,
+    feature_columns_ground,
+) = separate_player_and_ground(train_labels, feature_columns_player)
+train_labels_player.reset_index(drop=True, inplace=True)
+train_labels_ground.reset_index(drop=True, inplace=True)
 
-train_labels, columns = add_product_and_difference_features(train_labels)
-feature_columns += columns
+del train_labels
+gc.collect()
 
-X_train = train_labels[feature_columns]
-y_train = train_labels["contact"]
+train_labels_player, columns = add_product_and_difference_features(train_labels_player)
+feature_columns_player += columns
 
-param = {
-    "objective": "binary",
-    "device_type": "gpu" if torch.cuda.is_available() else "cpu",
-    "seed": 42,
-    "force_row_wise": True,
-    "metric": "auc",
-}
-
-skf = StratifiedKFold(shuffle=True, random_state=42)
-
-models = []
-scores = []
-for i, (train_index, test_index) in enumerate(skf.split(X_train, y_train)):
-    print(f"Fold {i + 1}:")
-
-    X_train_fold, y_train_fold = X_train.loc[train_index], y_train[train_index]
-    X_test_fold, y_test_fold = X_train.loc[test_index], y_train[test_index]
-
-    train_data = lgb.Dataset(X_train_fold, label=y_train_fold)
-    validation_data = lgb.Dataset(X_test_fold, label=y_test_fold, reference=train_data)
-
-    model = lgb.train(
-        param,
-        train_data,
-        num_boost_round=20_000,
-        valid_sets=[validation_data],
-        callbacks=[lgb.early_stopping(stopping_rounds=100)],
-    )
-
-    y_pred = model.predict(X_test_fold, num_iteration=model.best_iteration)
-    score = roc_auc_score(y_test_fold, y_pred)
-
-    models.append(model)
-    scores.append(score)
+models_player = train_lightgbm(
+    train_labels_player[feature_columns_player], train_labels_player["contact"]
+)
+models_ground = train_lightgbm(
+    train_labels_ground[feature_columns_ground], train_labels_ground["contact"]
+)
 
 del (
-    X_train_fold,
-    train_baseline_helmets,
-    train_index,
-    train_labels,
     train_player_tracking,
+    train_baseline_helmets,
     train_video_metadata,
-    y_pred,
-    y_test_fold,
-    y_train_fold,
+    is_close,
 )
 gc.collect()
 
 sample_submission = split_contact_id(sample_submission)
 sample_submission = join_datetime_to_labels(sample_submission, test_player_tracking)
 
-sample_submission, feature_columns = create_features(
+sample_submission, feature_columns_player = create_features(
     sample_submission, test_player_tracking, test_baseline_helmets, test_video_metadata
 )
 
-sample_submission, columns = add_product_and_difference_features(sample_submission)
-feature_columns += columns
+(
+    sample_submission_player,
+    sample_submission_ground,
+    feature_columns_ground,
+) = separate_player_and_ground(sample_submission, feature_columns_player)
 
-X_test = sample_submission[feature_columns]
-y_pred_train = np.zeros(len(X_train))
-y_pred_test = np.zeros(len(X_test))
+# Do not reset index. We rather want to keep the order of sample_submission.csv
+# sample_submission_player.reset_index(drop=True, inplace=True)
+# sample_submission_ground.reset_index(drop=True, inplace=True)
 
-for model in models:
-    y_pred_train += model.predict(X_train, num_iteration=model.best_iteration) / len(
-        models
-    )
-    y_pred_test += model.predict(X_test, num_iteration=model.best_iteration) / len(
-        models
-    )
+sample_submission_player, columns = add_product_and_difference_features(
+    sample_submission_player
+)
+feature_columns_player += columns
 
-x0 = [0.5]
-res = minimize(matthews_corrcoef_, x0, args=(y_train, y_pred_train))
+sample_submission_player["contact"] = predict_lightgbm(
+    models_player,
+    train_labels_player[feature_columns_player],
+    train_labels_player["contact"],
+    sample_submission_player[feature_columns_player],
+)
+sample_submission_ground["contact"] = predict_lightgbm(
+    models_ground,
+    train_labels_ground[feature_columns_ground],
+    train_labels_ground["contact"],
+    sample_submission_ground[feature_columns_ground],
+)
 
-sample_submission["contact"] = (y_pred_test > res.x[0]).astype("int")
-sample_submission.loc[sample_submission["distance"] > DISTANCE_THRESHOLD, "contact"] = 0
+sample_submission = pd.concat([sample_submission_player, sample_submission_ground])
+sample_submission.sort_index(inplace=True)
+
+is_close = get_indices_of_closer_than_threshold(
+    sample_submission, feature_columns_player
+)
+sample_submission.loc[~is_close, "contact"] = 0
+
 sample_submission[["contact_id", "contact"]].to_csv(
     os.path.join(PATH_TO_OUTPUT, SUBMISSION), index=False
 )
+
+# Visualize feature importance
+fig, axs = plt.subplots(2, figsize=(6.4 * 5, 4.8 * 5))
+lgb.plot_importance(
+    models_player[0],
+    ax=axs[0],
+    title="Feature importance of player",
+)
+lgb.plot_importance(
+    models_ground[0],
+    ax=axs[1],
+    title="Feature importance of ground",
+)
+plt.show()
